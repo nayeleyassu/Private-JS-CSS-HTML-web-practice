3601e3502795ab0534d253d11d0bba4b76c357ba
Nayel Eyassu <111699900+nayeleyassu@users.noreply.github.com>
2023-11-29 19:59:28 +0300

    Update web.js

---
+++ website/web.js
@@ -0,0 +1,7 @@
+//var ClickMebutton = document.createElement('Button');
+//ClickMeButton.innerHTML = 'CLICK ME';
+//ClickMeButton.style.background = "#FFFFF"
+//document.body.appendChild(ClickMeButton)
+
+
+// Original button funtion code using JS before making improvements and changes on the html code
