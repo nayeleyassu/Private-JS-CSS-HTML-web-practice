5e3fa77a751109e849b047b51e01b926b9fc176f
Nayel Eyassu <111699900+nayeleyassu@users.noreply.github.com>
2023-12-01 09:39:43 +0300

    Add files via upload

---
+++ IDEA FOR THE REST OF THE WEBPAGE.js
@@ -0,0 +1,4 @@
+//The idea for the next webpage is to make a online recipe website that features many various recipes. 
+// Including many different catagories for breakfast, lunch, dinner or different cultures foods, desserts 
+//implement different types of functions and make it very user friendly and interactive. 
+// e.g add functions, filters, images so on. 
